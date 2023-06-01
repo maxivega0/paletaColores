d548335cd300ed51ada5596f7e1f86119b8ac18b
maxivega0 <maximilianovega600@gmail.com>
2023-06-01 18:51:03 -0300

    agregado del footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ function App() {
 
   return (
     <>
-    <Container>
+    <Container className="mb-5">
       <h1 className='display-4 text-center'>Guardar colores</h1>
       <FormularioColores></FormularioColores>
     </Container>
+    <footer className='text-center bg-dark text-light py-4'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
